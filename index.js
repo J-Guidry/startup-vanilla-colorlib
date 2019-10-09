3bcfc8d0e9ad053250cdba3b85a134acad461b7e
Guidry <jh_guidry@outlook.com>
2019-10-09 10:24:56 -0500

    made navbar responsive with mobile nav menu

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ function toggle(){
 window.addEventListener("scroll", toggle, {passive: true});
 window.addEventListener("touchmove", toggle, {passive: true});
 
+const navButton = document.querySelector(".toggle-mobile-nav");
+const navMenu = document.querySelector(".mobile-nav");
+const overlay = document.querySelector(".mobile-overlay");
+const navIcon = navButton.firstElementChild;
+
+function toggleMobileNav(){
+    navMenu.classList.toggle("is-active");
+    overlay.classList.toggle("is-active");
+    // console.log(navIcon.classList.contains("lnr-menu"));
+    if (navIcon.classList.contains("lnr-menu")){
+        navIcon.classList.remove("lnr-menu");
+        navIcon.classList.add("lnr-cross");
+    } else {
+        navIcon.classList.remove("lnr-cross");
+        navIcon.classList.add("lnr-menu");
+    }
+
+}
+navButton.addEventListener("click", toggleMobileNav);
+
